3ac433097c67ac918ea49a957e788ce64d2d8c1c
JP <1.4.gigz@gmail.com>
2019-02-24 19:10:10 -0600

    Add test method. Closes #2.

--- src/test/java/com/fasoho/tddjava/MailTemplateTest.java
+++ src/test/java/com/fasoho/tddjava/MailTemplateTest.java
@@ -1,5 +1,9 @@
 package com.fasoho.tddjava;
 
+import static org.junit.jupiter.api.AssertEquals.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * @project tddpractice
  * @user jp
@@ -7,6 +11,11 @@ package com.fasoho.tddjava;
  */
 public class MailTemplateTest {
 
-  
+  @Test
+  public void oneVariable() throws Exception {
+    MailTemplate mailTemplate = new MailTemplate("Hello, ${name}");
+    mailTemplate.set("name", "Reader");
+    assertEquals("Hello, Reader", mailTemplate.evaluate());
+  }
 
 }
