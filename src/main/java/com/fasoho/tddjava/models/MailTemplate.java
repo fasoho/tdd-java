690107b54c2830b85d4f4c8e618342e17c50f1ec
JP <1.4.gigz@gmail.com>
2019-02-24 19:14:58 -0600

    Write test. Closes #3.

---
+++ src/main/java/com/fasoho/tddjava/models/MailTemplate.java
@@ -0,0 +1,20 @@
+package com.fasoho.tddjava.models;
+
+/**
+ * @project tddpractice
+ * @user jp
+ * @date 2019-02-24
+ */
+public class MailTemplate {
+
+  public MailTemplate(String templateText) {
+    
+  }
+
+  public void set(String name, String reader) {
+  }
+
+  public Object evaluate() {
+    return null;
+  }
+}
--- src/test/java/com/fasoho/tddjava/MailTemplateTest.java
+++ src/test/java/com/fasoho/tddjava/MailTemplateTest.java
@@ -1,7 +1,8 @@
 package com.fasoho.tddjava;
 
-import static org.junit.jupiter.api.AssertEquals.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.fasoho.tddjava.models.MailTemplate;
 import org.junit.jupiter.api.Test;
 
 /**
